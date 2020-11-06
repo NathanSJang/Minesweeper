bc1d6ee36891d869a659070503b90ec837daa4d1
Jang <sbtan1989@gmail.com>
2020-11-06 15:40:40 -0800

    make random array

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 /*----- app's state (variables) -----*/
 let boxes;
-let bomb;
+let bombs;
 
 
 
@@ -17,21 +17,28 @@ const board = document.querySelector('.board');
 /*----- functions -----*/
 init();
 
-// function makeboard() {
-//   for (let i = 0; i < width * width; i++) {
-//     const box = document.createElement('div');
-//     box.setAttribute('id', i);
-//     board.appendChild(box);
-//     boxes.push(box);
-//   }
-// }
+
+
+function fillArray() {
+  let bombArray = Array(bombs).fill('bomb');
+  let goodArray = Array(49 - bombs).fill('good');
+  let gameArray = goodArray.concat(bombArray);
+  return boxes = gameArray;
+}
+
+function random() {
+  return boxes.sort(() => Math.random() - 0.5);
+}
 
 function render() {
 
+  fillArray();
+  random();
 }
 
 function init() {
   boxes = [];
-  
+  bombs = 10;
+
   render();
 }
