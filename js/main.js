a8360f781a91da2dda8496f61931c0825a3a0a13
Jang <sbtan1989@gmail.com>
2020-11-08 12:30:56 -0800

    still working

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let width;
 /*----- cached element references -----*/
 const board = document.querySelector('.board');
 const boxEl = document.querySelectorAll('.board > div');
-const box = boxEl.forEach(box => box);
+const msgEl = document.getElementById('msg');
 
 /*----- event listeners -----*/
 boxEl.forEach(box => {box.addEventListener('click', handleClick)})
@@ -24,12 +24,14 @@ function handleClick(e) {
   const box = Array.from(boxEl);
   const index = box.indexOf(e.target);
   const clickedBox = boxEl[index]
+  const currentId = clickedBox.id;
+  // console.log(clickedBox);
   
   if(gameStatus) return;
   if(clickedBox.classList.contains('check')) return;
   
   if (clickedBox.classList.contains('bomb')) {
-    console.log('Game over');
+    gameStatus = false;
   } else if(clickedBox.classList.contains('good')) {
     countNum();
     
@@ -37,11 +39,14 @@ function handleClick(e) {
     if (num != 0) {
       clickedBox.classList.add('check');
       clickedBox.innerHTML = num;
-      return;
     }
-    clickedBox.classList.add('check');
+  return clickedBox.classList.add('check'); 
   }
+  gameStatus = winMine();
+
+  render();
 }
+
 
 function placeBomb() {
   let leng = boxEl.length 
@@ -60,7 +65,7 @@ function countNum() {
   for(let i =0; i < boxEl.length; i++) {
     let countNum =0;
 
-    const leftBOX = ( i % width === 0);
+    const leftBOX = (i % width === 0);
     const rightBox = (i % width === -1);
       
     if ( i > 0 && !leftBOX && boxEl[i - 1].classList.contains('bomb') ) countNum++; //left
@@ -75,23 +80,76 @@ function countNum() {
   } 
 }
 
-function checkNearBox() {
-  const leftBOX = ( i % width === 0);
-  const rightBox = (i % width === -1);
-  const 
+function checkNearBox(clickedBox, currentId) {
+  const leftBOX = ( currentId % width === 0);
+  const rightBox = (currentId % width === -1);
 
+  if (currentId > 0 && !leftBOX) {
+    const newId = clickedBox[currentId -1].id;
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 6 && !rightBox) {
+    const newId = clickedBox[currentId +1 - width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 7) {
+    const newId = clickedBox[currentId - width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 8 && !leftBox) {
+    const newId = clickedBox[currentId -1 - width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 48 && !rightBox) {
+    const newId = clickedBox[currentId +1]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 42 && !leftBox) {
+    const newId = clickedBox[currentId -1 +width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 41) {
+    const newId = clickedBox[currentId +1 +width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
+  if (currentId > 40 && !rightBox) {
+    const newId = clickedBox[currentId + width]
+    const newBox = document.getElementById(newId);
+    countNum(newBox);
+  }
 }
 
+function winMine() {
+  if(bombs === 0) gameStatus === true;
+}
+
+function renderMessage() {
+  if (gameStatus === null) {
+    msgEl.textContent = `Let's Find the Bomb!`
+  } else if (gameStatus) {
+    msgEl.textContent = `You win`
+  } else {
+    msgEl.textContent = `You Lose`;
+  }
+}
 
 function render() {
 
   placeBomb();
+  renderMessage();
 }
 
 function init() {
   bombs = 10;
   width = 7;
-  gameStatus = false;
+  gameStatus = null;
 
   render();
 }
