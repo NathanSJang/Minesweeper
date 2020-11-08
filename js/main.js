e5d2e8e10ad707376c66064b3b5d1c8de4ad4a58
Jang <sbtan1989@gmail.com>
2020-11-08 10:24:48 -0800

    add check on click

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let width;
 /*----- cached element references -----*/
 const board = document.querySelector('.board');
 const boxEl = document.querySelectorAll('.board > div');
-
+const box = boxEl.forEach(box => box);
 
 /*----- event listeners -----*/
 boxEl.forEach(box => {box.addEventListener('click', handleClick)})
@@ -23,12 +23,22 @@ function handleClick(e) {
   const box = Array.from(boxEl);
   const index = box.indexOf(e.target);
   const clickedBox = boxEl[index]
-  console.log(clickedBox);
+  
+  if(gameStatus) return
+  if(clickedBox.classList.contains('check')) return
   
   if (clickedBox.classList.contains('bomb')) {
     console.log('Game over');
-  } if(clickedBox.classList.contains('good')) {
+  } else if(clickedBox.classList.contains('good')) {
     countNum();
+    
+    let num = clickedBox.getAttribute('data');
+    if (num != 0) {
+      clickedBox.classList.add('check');
+      clickedBox.innerHTML = num;
+      return;
+    }
+    clickedBox.classList.add('check')
   }
 }
 
@@ -48,22 +58,26 @@ function placeBomb() {
 function countNum() {
   for(let i =0; i < boxEl.length; i++) {
     let countNum =0;
-    console.log(boxEl[i])
+
     const leftBOX = ( i % width === 0);
     const rightBox = (i % width === -1);
       
-    if ( i > 0 && !leftBOX && boxEl[i - 1].classList.contains('bomb') ) countNum++ //left
-    if ( i > 6 && !rightBox && boxEl[i + 1 - width].classList.contains('bomb') ) countNum++ // top-right
-    if ( i > 7 && boxEl[i - width].classList.contains('bomb') ) countNum++ //top
-    if ( i > 8 && !leftBOX && boxEl[i - 1 - width].classList.contains('bomb') ) countNum++ //top-left
-    if ( i < 48 && !rightBox && boxEl[i + 1].classList.contains('bomb') ) countNum++//right
-    if ( i < 42 && !leftBOX && boxEl[i - 1 + width].classList.contains('bomb') ) countNum++//bottom=left
-    if ( i < 41 && !rightBox && boxEl[i + 1 + width].classList.contains('bomb') ) countNum++//bottom
-    if ( i < 40 && boxEl[i + width].classList.contains('bomb')) countNum ++// bottom
+    if ( i > 0 && !leftBOX && boxEl[i - 1].classList.contains('bomb') ) countNum++; //left
+    if ( i > 6 && !rightBox && boxEl[i + 1 - width].classList.contains('bomb') ) countNum++; // top-right
+    if ( i > 7 && boxEl[i - width].classList.contains('bomb') ) countNum++; //top
+    if ( i > 8 && !leftBOX && boxEl[i - 1 - width].classList.contains('bomb') ) countNum++; //top-left
+    if ( i < 48 && !rightBox && boxEl[i + 1].classList.contains('bomb') ) countNum++;//right
+    if ( i < 42 && !leftBOX && boxEl[i - 1 + width].classList.contains('bomb') ) countNum++;//bottom=left
+    if ( i < 41 && !rightBox && boxEl[i + 1 + width].classList.contains('bomb') ) countNum++;//bottom
+    if ( i < 40 && boxEl[i + width].classList.contains('bomb')) countNum ++;// bottom
     boxEl[i].setAttribute('data', countNum);
-    console.log(boxEl[i]);
   } }
 
+// function showNum(box) {
+//   let num = box.getAttribute('data');
+//   box.classList.add('checked')
+//   box.innerHTML = num;
+// }
 
 function render() {
 
